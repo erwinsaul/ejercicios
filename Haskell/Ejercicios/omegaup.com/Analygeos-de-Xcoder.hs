efd17b7cd7ab61c616a46602e7ae672b19102e6a
Erwin Saul <mackonkey_2@hotmail.com>
2025-12-14 00:32:29 -0400

    Practicando Haskell

--- Haskell/Ejercicios/omegaup.com/Analygeos-de-Xcoder.hs
+++ Haskell/Ejercicios/omegaup.com/Analygeos-de-Xcoder.hs
@@ -1,24 +1,54 @@
-semiperimetro :: Int -> Int -> Int -> Int -> Int -> Int -> Double
+-- Error de Ejecucion 
+import Text.Printf
 
-perimetro :: Int -> Int -> Int -> Int -> Int -> Int -> Double
+distancia :: Integer -> Integer -> Integer -> Integer -> Double
+distancia x1 y1 x2 y2 = sqrt (dx*dx + dy*dy)
+  where
+    dx = fromIntegral (x2 - x1) :: Double
+    dy = fromIntegral (y2 - y1) :: Double
 
-area :: Int -> Int -> Int -> Int -> Int -> Int -> Double
+semiperimetro :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Double
+semiperimetro xa ya xb yb xc yc = perimetro xa ya xb yb xc yc / 2
 
-recta :: Int -> Int -> Int -> Int -> Int -> Int -> String
+perimetro :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Double
+perimetro xa ya xb yb xc yc = ab + bc + ca
+  where
+    ab = distancia xa ya xb yb
+    bc = distancia xb yb xc yc
+    ca = distancia xc yc xa ya
+
+area :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Double
+area xa ya xb yb xc yc = fromIntegral (abs det) / 2
+  where
+    det = xa * (yb - yc) + xb * (yc - ya) + xc * (ya - yb)
+
+recta :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> String
+recta xa ya xb yb xc yc 
+  | ya == yb && yb == yc = "Recta paralela al eje x"
+  | xa == xb && xb == xc = "Recta paralela al eje y"
+  | otherwise            = ""
 
 main :: IO()
 main = do
     input <- getLine
-    let [xa, ya] = map read $ words input :: [Int]
+    let [xa, ya] = map read $ words input :: [Integer]
     input <- getLine
-    let [xb, yb] = map read $ words input :: [Int]
+    let [xb, yb] = map read $ words input :: [Integer]
     input <- getLine
-    let [xc, yc] = map read $ words input :: [Int]
+    let [xc, yc] = map read $ words input :: [Integer]
     let sp = semiperimetro xa ya xb yb xc yc
-    let p = perimetro xa ya xb yb xc yc
-    let a = area xa ya xb yb xc yc
-    let r = recta xa ya xb yb xc yc
-    print sp
-    print p
-    print a
-    print r
+        p = perimetro xa ya xb yb xc yc
+        a = area xa ya xb yb xc yc
+        r = recta xa ya xb yb xc yc
+    
+    if r /= "" then do
+        print (round sp :: Integer)
+        print (round p :: Integer)
+        print ( 0 :: Integer)
+        putStrLn r
+    
+    else do
+        printf "%.5f\n" sp
+        printf "%.4f\n" p
+        print (round a :: Integer)
+
